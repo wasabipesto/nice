7dbd670602ecdefdcbd9c5d15a4ffb044e45788c
wasabipesto <21313833+wasabipesto@users.noreply.github.com>
2025-08-27 11:10:02 -0400

    fastest submissions leaderboard

--- schema/functions.sql
+++ schema/functions.sql
@@ -24,3 +24,18 @@ WHERE
     AND s.disqualified = false
 GROUP BY s.username
 ORDER BY total_range DESC;
+
+-- Leaderboard for fastest submissions in the last 30 days
+SELECT
+    s.username,
+    (s.submit_time - c.claim_time) as duration,
+    s.search_mode,
+    ROUND(f.range_size / EXTRACT(EPOCH FROM (s.submit_time - c.claim_time)) / 1000000, 2) as speed_e6
+FROM submissions s
+JOIN claims c ON s.claim_id = c.id
+JOIN fields f ON s.field_id = f.id
+WHERE
+    s.submit_time >= CURRENT_DATE - INTERVAL '30 days'
+    AND s.disqualified = false
+ORDER BY speed_e6 DESC
+LIMIT 10;
