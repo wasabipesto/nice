c10f460fa1d34fd1b2e22f477b4ce1082bb7bd00
wasabipesto <21313833+wasabipesto@users.noreply.github.com>
2026-02-03 17:10:35 -0500

    add some more benchmarks

--- common/src/benchmark.rs
+++ common/src/benchmark.rs
@@ -5,6 +5,8 @@ use super::*;
 /// Different benchmark strategies.
 #[derive(Debug, Copy, Clone, ValueEnum)]
 pub enum BenchmarkMode {
+    /// Checking on base ten with a known nice number.
+    BaseTen,
     /// The default benchmark range: 1e6 @ base 40.
     Default,
     /// A large benchmark range: 1e8 @ base 40.
@@ -12,33 +14,37 @@ pub enum BenchmarkMode {
     /// A very large benchmark range: 1e9 @ base 40.
     /// This is the size of a typical field from the server.
     ExtraLarge,
+    /// A massive range, much larger than any field you would get from the API:
+    /// 1e13 @ base 50.
+    Massive,
     /// A benchmark range at a higher range: 1e6 @ base 80.
     HiBase,
 }
 
 pub fn get_benchmark_field(mode: BenchmarkMode) -> DataToClient {
     let base = match mode {
+        BenchmarkMode::BaseTen => 10,
         BenchmarkMode::Default => 40,
         BenchmarkMode::Large => 40,
         BenchmarkMode::ExtraLarge => 40,
+        BenchmarkMode::Massive => 50,
         BenchmarkMode::HiBase => 80,
     };
-    let range_start = base_range::get_base_range_u128(base)
-        .unwrap()
-        .unwrap()
-        .range_start;
+    let base_range = base_range::get_base_range_u128(base).unwrap().unwrap();
     let range_size = match mode {
+        BenchmarkMode::BaseTen => base_range.range_size,
         BenchmarkMode::Default => 1_000_000,
         BenchmarkMode::Large => 100_000_000,
         BenchmarkMode::ExtraLarge => 1_000_000_000,
-        BenchmarkMode::HiBase => 1_000_000,
+        BenchmarkMode::Massive => 1e13 as u128,
+        BenchmarkMode::HiBase => 1_000_000_000,
     };
 
     DataToClient {
         claim_id: 0,
         base,
-        range_start,
-        range_end: range_start + range_size,
+        range_start: base_range.range_start,
+        range_end: base_range.range_start + range_size,
         range_size,
     }
 }
