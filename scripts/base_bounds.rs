44ce0e3d0002db9bc90252d3c9f09090caa60e3b
wasabipesto <21313833+wasabipesto@users.noreply.github.com>
2026-02-19 08:49:48 -0500

    Show max subdivision depth in base_bounds script

--- scripts/base_bounds.rs
+++ scripts/base_bounds.rs
@@ -5,6 +5,7 @@
 //! ```
 
 use nice_common::base_range::get_base_range_u128;
+use nice_common::msd_prefix_filter::MSD_RECURSIVE_MIN_RANGE_SIZE;
 
 fn main() {
     for base in 10..=97 {
@@ -16,6 +17,15 @@ fn main() {
                     end = base_range.end(),
                     size = base_range.size(),
                 );
+
+                // Calculate depth needed to subdivide base range into min_range
+                let msd_min_range_size = 10_000;
+                let depth = (base_range.size() as f64 / msd_min_range_size as f64)
+                    .log2()
+                    .ceil() as u32;
+                println!(
+                    "         Subdivision depth to hit min range {msd_min_range_size:.0e}: 2^{depth}"
+                )
             }
             None => {
                 continue;
